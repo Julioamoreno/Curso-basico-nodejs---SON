128d149d0aff5e16e0c8486fb377614e8b32f474
Júlio Armando <julioarmando321@gmail.com>
2019-11-29 16:32:14 -0300

    Aula 6- Trabalhando com http

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log('Hello World')
+var http = require('http')
+
+function handle(req,res){
+    res.writeHead(200,{
+        'Content-Type':'text/html'
+    })
+
+    res.write('<!DOCTYPE "html">')
+    res.write('<html>')
+    res.write('<head><title> Http Module</title></head>')
+    res.write('<body>')
+    res.write('<h1> Hello </h1>')
+    res.write('</body>')
+    res.write('</html>')
+    
+    res.end()
+}
+
+var server = http.createServer(handle)
+
+server.listen(3000,function(){
+    console.log('Server rodando na porta 3000')
+})
